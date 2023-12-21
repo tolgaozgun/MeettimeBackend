0be80c0c58d9e2f481b5229f7847fd07332a7ef9
Tolga Ozgun <tolgaozgunn@gmail.com>
2023-12-21 17:55:01 +0300

    Project initialization

---
+++ src/main/java/com/tolgaozgun/meettime/MeetTimeApplication.java
@@ -0,0 +1,13 @@
+package com.tolgaozgun.meettime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tolgaozgun/meettime/MeetTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolgaozgun.meettime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
